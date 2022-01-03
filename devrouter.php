bcc749758be00ee00e4a318953d2cf0c2e0381a6
Virgil Dupras <virgil.dupras@toumoro.com>
2022-01-03 10:19:56 -0500

    Add dummy theme

--- devrouter.php
+++ devrouter.php
@@ -1,6 +1,10 @@
 <?php
 if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["SCRIPT_FILENAME"])) {
     return false;    // serve the requested resource as-is.
+} else if (preg_match('/devrouter\.php$/', $_SERVER["SCRIPT_FILENAME"])) {
+    // Something's wrong
+    error_log(print_r($_SERVER));
+    die;
 } else if (preg_match('/\.php$/', $_SERVER["SCRIPT_FILENAME"])) {
     require $_SERVER["SCRIPT_FILENAME"];
 } else {
